daef782d6a34c20088782f04dcf2ed6f4da0e0fd
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-08-12 23:47:02 +0200

    Fix Vercel auto deployment failing: Update Next.js configuration for experimental ESM support and adjust TypeScript module resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        esmExternals: 'loose'
+    },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+        };
+        return config;
+    },
 }
 
 export default nextConfig
